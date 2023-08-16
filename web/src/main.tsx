80fb7307896a39392abc20cdfbb94dc2e5efd0f9
Igor Andrade <rogiandradee@gmail.com>
2023-08-16 21:17:59 +0100

    feat(implement): :sparkles: implement new features, hooks that change the darkmode and the language.

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,8 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { DarkModeProvider } from './contexts/colorsProvider.tsx'
 import App from './App.tsx'
+import { HeaderProvider } from './contexts/headerProvider.tsx'
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <DarkModeProvider>
+      <HeaderProvider>
+        <App />
+      </HeaderProvider>
+    </DarkModeProvider>
   </React.StrictMode>,
 )
