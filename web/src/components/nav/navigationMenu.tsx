afe10f7779c105eb98d4d51c292ff68e4548d747
Igor Andrade <rogiandradee@gmail.com>
2023-10-16 20:29:18 +0100

    Update navigationMenu.tsx
    - Add woodwork project

--- web/src/components/nav/navigationMenu.tsx
+++ web/src/components/nav/navigationMenu.tsx
@@ -2,6 +2,33 @@ import * as NavigationMenu from '@radix-ui/react-navigation-menu';
 import * as DropdownMenu from '@radix-ui/react-dropdown-menu';
 import { CaretDownIcon } from '@radix-ui/react-icons';
 
+export function NavigationMenuWoodwork() {
+
+    return(
+        <NavigationMenu.Root className='navigationMenuRoot'>
+                <NavigationMenu.List className='navigationMenuList'>
+                    <NavigationMenu.Item>
+                        <NavigationMenu.Trigger className='navigationMenuTrigger'>
+                            <CaretDownIcon className='caretDown' aria-hidden />
+                        </NavigationMenu.Trigger>
+                        <NavigationMenu.Content className='navigationMenuContentVLins'>
+                            <NavigationMenu.Link>
+                                <a
+                                    className='links'
+                                    href='https://github.com/rogiandrade/e-commerce-woodwork'
+                                    target='_blank'
+                                >
+                                    <img className='linkIcon' src="github-logo.svg" alt="globe" />
+                                    GitHub
+                                </a>
+                            </NavigationMenu.Link>
+                        </NavigationMenu.Content>
+                    </NavigationMenu.Item>
+                </NavigationMenu.List>
+            </NavigationMenu.Root>
+    )
+}
+
 export function NavigationMenuVLins() {
 
     return(
