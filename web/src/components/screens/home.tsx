df288760dcbed23ab7ab616c3ac0d3c430d53f7d
Igor Andrade <rogiandradee@gmail.com>
2024-01-02 22:23:14 +0000

    Update home.tsx

--- web/src/components/screens/home.tsx
+++ web/src/components/screens/home.tsx
@@ -1,21 +1,25 @@
 import { changeLocale } from "../../contexts/changeLocaleProvider";
 
 export function Home() {
-
-    return (
-        <>
-            <section className="home">
-                <div className="homeBox">
-                    <div className="homeText">
-                        <h1
-                            className="homeTitle"
-                            dangerouslySetInnerHTML={{ __html: changeLocale('home', 'title') }} />
-                        <p className="homeSubTitle">{changeLocale('home', 'subtitle')}</p>
-                    </div>
-                    <img className="imgHome" src="profile-pic.png" alt="logo" />
-                </div>
-            </section>
-        </>
-    )
-
+  return (
+    <>
+      <section className="home">
+        <div className="homeBox">
+          <div className="homeText">
+            <h1
+              className="homeTitle"
+              dangerouslySetInnerHTML={{
+                __html: changeLocale("home", "title"),
+              }}
+            />
+            <p className="homeSubTitle">{changeLocale("home", "subtitle")}</p>
+          </div>
+          <div className="imgHomeContent">
+            <div className="imgHomeBlur" />
+              <img className="imgHome" src="imageHome.png" />
+          </div>
+        </div>
+      </section>
+    </>
+  );
 }
