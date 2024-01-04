a735038a398a53280321e940d5e453168353b98e
Igor Andrade <rogiandradee@gmail.com>
2024-01-04 21:28:53 +0000

    Update home.tsx

--- web/src/components/screens/home.tsx
+++ web/src/components/screens/home.tsx
@@ -12,10 +12,10 @@ export function Home() {
                 __html: changeLocale("home", "title"),
               }}
             />
+            <br/>
             <p className="homeSubTitle">{changeLocale("home", "subtitle")}</p>
           </div>
           <div className="imgHomeContent">
-            <div className="purpleCircle" />
               <img className="imgHome" src="imageHome.png" />
           </div>
         </div>
